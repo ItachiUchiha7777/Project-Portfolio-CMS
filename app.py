e1c19951dd771d7bad6255ab4f3d1372c56cbe90
Shanmukha <rohitgusain792@gmail.com>
2024-05-03 23:17:46 +0530

    tempaltes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import secrets
 import random
 
 from datetime import datetime
+
 app = Flask(__name__, static_folder="static")
 app.config["SECRET_KEY"] = "chiaggadiigga"
 
@@ -19,72 +20,81 @@ Session(app)
 connect = sqlite3.connect("database.db")
 
 
-
 UPLOAD_FOLDER = "static/uploads"
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-
-user=""
-
+user = ""
 
 
-@app.route("/")
-def index(str): 
-   
+@app.route("/<string:name>")
+def index(name):
     with sqlite3.connect("database.db") as connect:
-            cursor = connect.cursor()
-            cursor.execute("SELECT * FROM posts")
-            data = cursor.fetchall()
-            # return (data)
-            return render_template("index.html", data=data)
-            
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM posts")
+        data = cursor.fetchall()
+
+        # Render the HTML template inside templates/<name>/name.html
+        temp = name + "/index.html"
+        global user
+        user = name
+
+        return render_template(temp, data=data)
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template(f"{user}/about.html")
+
 
 @app.route("/resume")
 def resume():
-    return render_template("resume.html")
+    return render_template(f"{user}/resume.html")
+
 
 @app.route("/services")
 def services():
     return render_template("services.html")
 
+
 @app.route("/portfolio")
 def portfolio():
     return render_template("portfolio.html")
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
 @app.route("/login")
 def lofinform():
     return render_template("login.html")
+
 
 @app.route("/admin")
 def admin():
     try:
         if session["name"]:
-             return render_template("admin.html")
+            return render_template("admin.html")
     except:
         pass
     return redirect(url_for("login"))
 
-    
 
 @app.route("/upload")
 def upload_form():
-    if session.get('name'):
-     return render_template("upload.html")
+    if session.get("name"):
+        return render_template("upload.html")
     else:
         return redirect(url_for("login"))
     # if session.get('username'):
     #      return render_template("upload.html")
     # else:
     #     return redirect(url_for("upload"))
+
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
@@ -96,11 +106,10 @@ def upload_file():
     title = request.form.get("title")
     dt = datetime.now()
 
-   
-    time = dt.strftime('%Y-%m-%d %H:%M')
+    time = dt.strftime("%Y-%m-%d %H:%M")
     if photo.filename == "":
         return "No selected file"
-    
+
     if photo:
         filename = secure_filename(photo.filename)
         extension = os.path.splitext(filename)[1]
@@ -114,7 +123,7 @@ def upload_file():
             cursor.execute(
                 """INSERT INTO posts  
                 (postID,media,title,caption,time) VALUES (?,?,?,?,?)""",
-                (randommm,new_filename,title, text,time),
+                (randommm, new_filename, title, text, time),
             )
             users.commit()
 
@@ -142,37 +151,23 @@ def loginform():
         return "Wrong Username"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 @app.route("/view/<postid>")
 def profile(postid):
-    
+
     connect = sqlite3.connect("database.db")
     cursor = connect.cursor()
 
     cursor.execute("SELECT * FROM posts WHERE postID=? ", (postid,))
-    data_in1 = cursor.fetchall()  
-    data_in=data_in1[0]
-    data={
-            "postid": data_in[0],
-            "media": data_in[1],
-            "title": data_in[2],
-            "content": data_in[3],
-            "date":data_in[4],
-        }
-    return render_template("view.html",data=data)
-
-
+    data_in1 = cursor.fetchall()
+    data_in = data_in1[0]
+    data = {
+        "postid": data_in[0],
+        "media": data_in[1],
+        "title": data_in[2],
+        "content": data_in[3],
+        "date": data_in[4],
+    }
+    return render_template("view.html", data=data)
 
 
 @app.route("/delete/<postid>")
@@ -181,12 +176,9 @@ def search_user(postid):
     cursor = connect.cursor()
 
     cursor.execute("SELECT * FROM users WHERE postID=?", (postid,))
-    data_in = cursor.fetchall()  
+    data_in = cursor.fetchall()
     return data_in
 
 
-
-
 if __name__ == "__main__":
-    app.run( debug=True)
-
+    app.run(debug=True)
